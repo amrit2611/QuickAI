d981875d738da0ea6d93ca1a72fa518c56697f1c
amrit <amrit@aviac.in>
2024-12-02 23:26:19 +0530

    minor ui change

--- src/components/aisearch.jsx
+++ src/components/aisearch.jsx
@@ -54,7 +54,7 @@ const AISearch = () => {
                         </div>
                     ) : (
                         searchResult && (
-                            <div className="border border-gray-400 rounded-lg w-4/6 mx-auto my-2 p-4">
+                            <div className="border border-gray-400 rounded-lg w-4/6 my-2 mx-auto p-4 flex">
                                 <ReactMarkDown>{searchResult}</ReactMarkDown>
                             </div>
                         )
